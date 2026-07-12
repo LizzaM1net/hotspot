f6bd6b9fd982ad93920d9704fe7ae1d9fe9b9ccf
Andrew X <a14365747@gmail.com>
2026-07-12 04:24:10 +0300

    use catch2 for testing

--- tests/test_hproto.cpp
+++ tests/test_hproto.cpp
@@ -1,3 +1,5 @@
+#include <catch2/catch_test_macros.hpp>
+
 #include <cassert>
 #include <cstdint>
 #include <hproto.h>
@@ -15,46 +17,42 @@ struct PaddedStruct {
 
 struct EmptyStruct {};
 
-int main() {
-    {
-        hLog() << "Testing SimpleStruct";
-        SimpleStruct simpleObj{1, 2, 3};
-        const char simpleExpected[] = {
-            0x01, 0x00, 0x00, 0x00,
-            0x02, 0x00, 0x00, 0x00,
-            0x03, 0x00, 0x00, 0x00,
-        };
-        char simpleReal[sizeof(SimpleStruct)];
-        hproto_write(simpleObj, simpleReal);
-        assert(memcmp(simpleReal, simpleExpected, sizeof(SimpleStruct)) == 0);
-    }
+TEST_CASE("hproto_write serializes SimpleStruct correctly", "[hproto]") {
+    hLog() << "Testing SimpleStruct";
+    SimpleStruct simpleObj{1, 2, 3};
+    const char simpleExpected[] = {
+        0x01, 0x00, 0x00, 0x00,
+        0x02, 0x00, 0x00, 0x00,
+        0x03, 0x00, 0x00, 0x00,
+    };
+    char simpleReal[sizeof(SimpleStruct)];
+    hproto_write(simpleObj, simpleReal);
+    CHECK(memcmp(simpleReal, simpleExpected, sizeof(SimpleStruct)) == 0);
+}
 
-    {
-        hLog() << "Testing PaddedStruct";
-        PaddedStruct paddedObj;
-        // Immitates uninitialized data before constructor run
-        std::memset(&paddedObj, 0xfe, sizeof(PaddedStruct));
-        std::construct_at(&paddedObj, 1, 2.0, 3);
-        const char paddedExpected[] = {
-            0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40,
-            0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-        };
-        char paddedReal[sizeof(PaddedStruct)];
-        hproto_write(paddedObj, paddedReal);
-        assert(memcmp(paddedReal, paddedExpected, sizeof(PaddedStruct)) == 0);
-    }
+TEST_CASE("hproto_write serializes PaddedStruct correctly, zeroing padding", "[hproto]") {
+    hLog() << "Testing PaddedStruct";
+    PaddedStruct paddedObj;
+    // Immitates uninitialized data before constructor run
+    std::memset(&paddedObj, 0xfe, sizeof(PaddedStruct));
+    std::construct_at(&paddedObj, 1, 2.0, 3);
+    const char paddedExpected[] = {
+        0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40,
+        0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    };
+    char paddedReal[sizeof(PaddedStruct)];
+    hproto_write(paddedObj, paddedReal);
+    CHECK(memcmp(paddedReal, paddedExpected, sizeof(PaddedStruct)) == 0);
+}
 
-    {
-        hLog() << "Testing EmptyStruct";
-        EmptyStruct emptyObj;
-        const char emptyExpected[] = {
-            0x00
-        };
-        char emptyReal[sizeof(EmptyStruct)];
-        hproto_write(emptyObj, emptyReal);
-        assert(memcmp(emptyReal, emptyExpected, sizeof(EmptyStruct)) == 0);
-    }
-
-    return 0;
+TEST_CASE("hproto_write serializes EmptyStruct correctly", "[hproto]") {
+    hLog() << "Testing EmptyStruct";
+    EmptyStruct emptyObj;
+    const char emptyExpected[] = {
+        0x00
+    };
+    char emptyReal[sizeof(EmptyStruct)];
+    hproto_write(emptyObj, emptyReal);
+    CHECK(memcmp(emptyReal, emptyExpected, sizeof(EmptyStruct)) == 0);
 }
--- tests/test_reflection.cpp
+++ tests/test_reflection.cpp
@@ -1,3 +1,5 @@
+#include <catch2/catch_test_macros.hpp>
+
 #include <hproto.h>
 #include <hlog.h>
 
@@ -5,19 +7,17 @@ struct StructA {};
 struct StructB {};
 struct StructC {};
 
-int main() {
-    assert(HProtoId<StructA>::name == "StructA");
-    assert(HProtoId<StructA>::id == 4476163269382220915ULL);
-    assert(HProtoId<StructB>::name == "StructB");
-    assert(HProtoId<StructB>::id == 4476164368893849126ULL);
-    assert(HProtoId<StructC>::name == "StructC");
-    assert(HProtoId<StructC>::id == 4476165468405477337ULL);
-    assert(HProtoId<std::string>::name == "std::string");
-    assert(HProtoId<std::string>::id == 2839446397891759191ULL);
-    assert(HProtoId<double>::name == "double");
-    assert(HProtoId<double>::id == 11567507311810436776ULL);
-    assert((HProtoId<std::variant<std::string, double>>::name == "std::variant<std::string, double>"));
-    assert((HProtoId<std::variant<std::string, double>>::id == 9686022488139303836ULL));
-
-    return 0;
+TEST_CASE("Type name and id are computed correctly for types", "[reflection]") {
+    CHECK(HProtoId<StructA>::name == "StructA");
+    CHECK(HProtoId<StructA>::id == 4476163269382220915ULL);
+    CHECK(HProtoId<StructB>::name == "StructB");
+    CHECK(HProtoId<StructB>::id == 4476164368893849126ULL);
+    CHECK(HProtoId<StructC>::name == "StructC");
+    CHECK(HProtoId<StructC>::id == 4476165468405477337ULL);
+    CHECK(HProtoId<std::string>::name == "std::string");
+    CHECK(HProtoId<std::string>::id == 2839446397891759191ULL);
+    CHECK(HProtoId<double>::name == "double");
+    CHECK(HProtoId<double>::id == 11567507311810436776ULL);
+    CHECK(HProtoId<std::variant<std::string, double>>::name == "std::variant<std::string, double>");
+    CHECK(HProtoId<std::variant<std::string, double>>::id == 9686022488139303836ULL);
 }
